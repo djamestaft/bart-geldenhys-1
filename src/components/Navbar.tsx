879381ec00f51c79355ffd4d1c33ee1af41d0fc3
Devon Taft <devon.taft@rank.com>
2022-07-17 23:29:26 +0200

    Added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
+import img4 from '../../public/assets/images/bg-logo.svg';
+console.log('🚀 ~ file: Navbar.tsx ~ line 4 ~ img4', img4)
 
 function Navbar() {
   return (
@@ -9,7 +11,7 @@ function Navbar() {
           <Link href="/"><a className="nav-link" href="">Home</a></Link>
           <Link href="/about"><a className="nav-link" href="">About</a></Link>
           <Link href="/gallery"><a className="nav-link" href="">Gallery</a></Link>
-          <Link href="/"><a className="nav-link text-xl text-center font-bold" href="">Bart Geldenhys</a></Link>
+          <Link href="/"><a className="nav-link  text-xl text-center font-bold" href=""><img className='logo-image' src={img4.src} alt="" /></a></Link>
           <Link href="/booking"><a className="nav-link" href="">Bookings</a></Link>
           <Link href="/faq"><a className="nav-link" href="">F.A.Q</a></Link>
           <Link href="/contact"><a className="nav-link" href="">Contact</a></Link>
