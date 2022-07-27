80d00edfebc77bcfd34281b54818e80f08e409d9
Devon Taft <devon.taft@rank.com>
2022-07-27 14:15:49 +0200

    Strapi (#3)
    * add strapi CMS
    
    * added cms
    
    * fix products default value
    
    * remove any type
    
    * Fixed some console errors
    
    * Fixed heading not coming through
    
    * Added pageSettings for home page
    
    * Added workshops types strapi
    
    * refinements
    
    * Added logo
    
    * tweaks
    
    * Added basic footer
    
    * and another one
    
    * Added cloudinary support
    
    * hello
    
    * asdasd
    
    Co-authored-by: Duane Cilliers <hello@duane.co.za>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
 import img4 from '../../public/assets/images/bg-logo.svg';
 console.log('🚀 ~ file: Navbar.tsx ~ line 4 ~ img4', img4)
@@ -11,7 +12,7 @@ function Navbar() {
           <Link href="/"><a className="nav-link" href="">Home</a></Link>
           <Link href="/about"><a className="nav-link" href="">About</a></Link>
           <Link href="/gallery"><a className="nav-link" href="">Gallery</a></Link>
-          <Link href="/"><a className="nav-link  text-xl text-center font-bold" href=""><img className='logo-image' src={img4.src} alt="" /></a></Link>
+          <Link href="/"><a className="nav-link  text-xl text-center font-bold" href=""><Image className='logo-image' src={img4.src} alt="" layout='fill' /></a></Link>
           <Link href="/booking"><a className="nav-link" href="">Bookings</a></Link>
           <Link href="/faq"><a className="nav-link" href="">F.A.Q</a></Link>
           <Link href="/contact"><a className="nav-link" href="">Contact</a></Link>
